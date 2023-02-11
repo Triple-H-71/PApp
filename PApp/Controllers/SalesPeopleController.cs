0a0dedfae35369e65c5d18ddd58f85306a767949
Triple-H-71 <hunter@fastmail.com>
2023-02-11 18:41:01 -0500

    Add project files.

---
+++ PApp/Controllers/SalesPeopleController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Dapper;
+using System.Diagnostics;
+using PApp.Models;
+using System.Data.SqlClient;
+
+namespace PApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SalesPeopleController : ControllerBase
+    {      
+
+        private readonly ILogger<SalesPeopleController> _logger;
+        private readonly IDBConnector _dbc;
+        
+
+        public SalesPeopleController(ILogger<SalesPeopleController> logger, IDBConnector dbc)
+        {
+            _logger = logger;
+            _dbc = dbc;
+        }
+
+        [HttpGet(Name = "GetAllSalesPeople")]
+        public List<Salesperson> Get()
+        {
+            List<Salesperson> salesPpl;
+            // var salesPpl = new List<Salesperson>();
+            using (var connection = new SqlConnection(_dbc.GetDatabaseName()))
+            {
+                // Create a query that retrieves all books with an author name of "John Smith"    
+                var sql = "SELECT * FROM Salesperson";
+
+                // Use the Query method to execute the query and return a list of objects    
+                salesPpl = connection.Query<Salesperson>(sql).ToList();
+            }
+
+            return salesPpl;            
+        }
+    }
+}
---
+++ PApp/DBConnector.cs
@@ -0,0 +1,17 @@
+﻿
+namespace PApp
+{
+    public interface IDBConnector
+    {
+        public string GetDatabaseName();
+    }
+    public class DBConnector : IDBConnector
+    {
+        public string GetDatabaseName()
+        {
+            var dbstring = "Server=tcp:hunterh.database.windows.net,1433;Initial Catalog=Main;Persist Security Info=False;User ID=hunterh_admin;Password=XsYXkPVnMn3JD54;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            return dbstring;
+        }
+    }
+}
